51e82aa4b3e271ebe5abde54f2e0c3c20657c419
github username <github email address>
2019-07-14 10:24:21 -0700

    unrefactoring to be react only

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,63 +1,67 @@
 import React from 'react';
 import Bowies from './Bowies';
+import babyBowie from '../assets/babyBowie.jpeg';
+import hipsterBowie from '../assets/hipsterBowie.jpg';
+import ziggyBowie from '../assets/ziggyBowie.jpg';
+import goblinBowie from '../assets/goblinBowie.jpg';
 
 
 class MainContainer extends React.Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     bowies: {
-  //       1: {
-  //         id: 1,
-  //         name: 'Baby Bowie',
-  //         votes: 0,
-  //         image: babyBowie
-  //       },        
-  //       2: {
-  //         id: 2,
-  //         name: 'Hipster Bowie',
-  //         votes: 0,
-  //         image: hipsterBowie
-  //       },
-  //       3: {
-  //         id: 3,
-  //         name: 'Ziggy Stardust',
-  //         votes: 0,
-  //         image: ziggyBowie
-  //       },
-  //       4: {
-  //         id: 4,
-  //         name: 'Goblin King',
-  //         votes: -1000,
-  //         image: goblinBowie
-  //       },
-  //     }
-  //   }
-  // }
+  constructor() {
+    super();
+    this.state = {
+      bowies: {
+        1: {
+          id: 1,
+          name: 'Baby Bowie',
+          votes: 0,
+          image: babyBowie
+        },        
+        2: {
+          id: 2,
+          name: 'Hipster Bowie',
+          votes: 0,
+          image: hipsterBowie
+        },
+        3: {
+          id: 3,
+          name: 'Ziggy Stardust',
+          votes: 0,
+          image: ziggyBowie
+        },
+        4: {
+          id: 4,
+          name: 'Goblin King',
+          votes: -1000,
+          image: goblinBowie
+        },
+      }
+    }
+  }
 
-  // increaseVote = (bowieId) => {
-  //   this.setState({
-  //     bowies: {
-  //       ...this.state.bowies,
-  //       [bowieId]: {
-  //         ...this.state.bowies[bowieId],
-  //         votes: this.state.bowies[bowieId]['votes'] + 1
-  //       }
-  //     }
-  //   })
-  // }
+  increaseVote = (bowieId) => {
+    this.setState({
+      bowies: {
+        ...this.state.bowies,
+        [bowieId]: {
+          ...this.state.bowies[bowieId],
+          votes: this.state.bowies[bowieId]['votes'] + 1
+        }
+      }
+    })
+  }
 
-  // decreaseVote = (bowieId) => {
-  //   this.setState({
-  //     bowies: {
-  //       ...this.state.bowies,
-  //       [bowieId]: {
-  //         ...this.state.bowies[bowieId],
-  //         votes: this.state.bowies[bowieId]['votes'] - 1
-  //       }
-  //     }
-  //   })
-  // }
+  decreaseVote = (bowieId) => {
+    this.setState({
+      bowies: {
+        ...this.state.bowies,
+        [bowieId]: {
+          ...this.state.bowies[bowieId],
+          votes: this.state.bowies[bowieId]['votes'] - 1
+        }
+      }
+    })
+  }
   
   render() {
     return (
--- src/index.js
+++ src/index.js
@@ -2,14 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
 
-import reducer from './reducers/combinedReducers'
-
-const store = createStore(reducer);
-console.log('here is your store', store.getState())
-
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-
-// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
