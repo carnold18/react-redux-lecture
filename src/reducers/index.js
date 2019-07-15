107d15ff555f00a6d236b6c6d2c62120971e5585
github username <github email address>
2019-07-14 19:27:05 -0700

    added comments on combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,6 +4,7 @@ import bowieReducer from './bowieReducer'
 
 // this will return another object that will point to 
 // our other objects when needed
+// name this file index since it hold all the reducers
 
 export default combineReducers({
     headerReducer,
